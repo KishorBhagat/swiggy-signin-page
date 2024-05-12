fbf34f210d1a6d594c287e7f7f66670389c59162
Kishor Bhagat <kishorebhagat663@gmail.com>
2024-05-12 20:07:12 +0530

    style: border color on input value

--- src/components/OtpInputBoxes.jsx
+++ src/components/OtpInputBoxes.jsx
@@ -9,7 +9,7 @@ function OtpInputBoxes({otp, setOtp}) {
       numInputs={6}
       inputType='number'
       shouldAutoFocus
-      renderInput={(props) => <input {...props} className='mx-auto md:mx-2 box-content h-10 w-10 px-3 text-lg border-b-2 border-gray-400 font-semibold focus:border-orange-500 focus:outline-none spin-button-none'/>}
+      renderInput={(props) => <input {...props} className={`mx-auto md:mx-2 box-content h-10 w-10 px-3 text-lg border-b-2 border-${props.value?"orange-500":"gray-400"} font-semibold focus:border-orange-500 focus:outline-none spin-button-none`}/>}
     />
   )
 }
